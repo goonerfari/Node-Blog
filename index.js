11d4da55817952aef0b01e19e1f74f8d5c9bdc81
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-04 19:07:07 -0500

    ADDED SERVER FILE, IMPORTED TO INDEX

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // code away!
 const express = require('express');
+const server = require('./server');
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`Listening to localhost: ${PORT}`);
+})
