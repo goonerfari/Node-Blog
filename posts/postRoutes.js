4e5f7f156c8d966920d4c66f8098ee96f5a7a400
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-05 20:27:29 -0500

    ADD DELETE POST ROUTE

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
             res.status(200).json(posts);
         }
         else {
-            res.status(404).json('There are no available posts')
+            res.status(404).json('There are no available posts.')
         }
     }
     catch (e) {
@@ -28,10 +28,10 @@ router.post('/', async (req, res) => {
 
     try {
         if (added) {
-            res.status(201).json('Item Added');
+            res.status(201).json('Item Added.');
         }
         else {
-            res.json('Please enter title and body');
+            res.json('Please enter title and body.');
         }
     }
     catch (e) {
@@ -47,37 +47,51 @@ router.get('/:postId', async (req, res) => {
             res.status(200).json(post);
         }
         else {
-            res.json('Post id is unavailable.')
+            res.status(404).json('Post id is unavailable.')
         }
     }
     catch (e) {
         res.status(500).json(e);
     }
 
-    console.log(post)
 })
 
 router.put('/:postId', async (req, res) => {
 
     const id = req.params.postId;
-    console.log(id);
     const body = req.body;
-    console.log(body);
     const newPost = await postDb.update(id, body);
-    // console.log(newPost);
 
     try {
         if (newPost) {
-            res.status(201).json('Item updated');
+            res.status(201).json('Item updated.');
         }
         else {
-            res.json('Post id is unavailable')
+            res.status(404).json('Post id is unavailable.')
         }
     }
     catch (e) {
         res.status(500).json(e);
     }
 })
+
+router.delete('/:postId', async (req, res) => {
+
+    const id = req.params.postId;
+    const deleted = await postDb.remove(id);
+
+    try {
+        if (deleted) {
+            res.status(200).json('Post successfully deleted.');
+        }
+        else {
+            req.status(400).json('Post id is unavailable.')
+        }
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+});
 
 
 module.exports = router;
