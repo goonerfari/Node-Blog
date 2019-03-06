b8c0c6b238c23c6dad8dd67d53379c56d6cee555
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-05 22:07:20 -0500

    RESTRUCTURE MIDDLEWARE, UPPERCASE USERNAME BEFORE POSTING USER

--- server.js
+++ server.js
@@ -2,43 +2,22 @@ const express = require('express');
 const server = express();
 const logger = require('morgan');
 const helmet = require('helmet');
-server.use(express.json(), logger('dev'), helmet(), upperCase);
+server.use(express.json(), logger('dev'), helmet());
 
 const postRoutes = require('./posts/postRoutes');
 const userRoutes = require('./users/userRoutes');
 
-function upperCase(req, res, next) {
-    // const password = req.headers.authorization;
 
-    const username = req.body.name
-    const modified = username;
-    modified[0] = modified[0].toUpperCase();
 
-    if (username === modified) {
-        next();
-    }
-    else {
-        next({
-            err: 'User is not capitalized'
-        })
-    }
-}
-server.use((err, req, res, next) => {
-  
-    res
-      .status(500)
-      .json({ 
-          message: 'There was an error performing the required operation',
-          err: err
-        });
-  });
+
 
 
 server.get('/', (req, res) => {
     res.status(200).json('Home Page up and running')
 });
 
-server.use('/api/posts/', upperCase, postRoutes);
-server.use('/api/users/', upperCase, userRoutes);
+server.use('/api/posts/', postRoutes);
+server.use('/api/users/', userRoutes);
+
 
 module.exports = server;
--- users/userRoutes.js
+++ users/userRoutes.js
@@ -3,35 +3,75 @@ const postDb = require('../data/helpers/postDb.js');
 const userDb = require('../data/helpers/userDb.js');
 const router = express.Router();
 
+router.use(express.json());
+
+function upperCase(req, res, next) {
+
+    const name = req.body.name;
+    // console.log(req.body);
+    const modified = name;
+    modified[0] = modified[0].toUpperCase();
+
+    if (name === modified) {
+        next();
+    }
+    else {
+        res.json({ 
+            message: 'User is not capitalized.',
+            err: 'User is not capitalized.'
+        })    
+    }
+}
+router.use((err, req, res, next) => {
+  
+    res
+      .status(500)
+      .json({ 
+          message: 'There was an error performing the required operation',
+          err: err
+        });
+});
 
 router.get('/', async (req, res) => {
 
-    const users = await userDb.get();
-
     try {
+        const users = await userDb.get();
+
         if (users) {
             res.status(200).json(users);
         }
         else {
+            console.log('here')
             res.status(404).json(`Users are not available.`)
         }
     }
     catch (e) {
-        res.status(500).json(e)
+        console.log(e)
+        res.status(500).json(e);
     }
 })
 
-// router.post('/', async (req, res) => {
+router.post('/', upperCase, async (req, res) => {
 
-//     const newUser = req.body;
+    const newUser = req.body.name;
 
-//     const added = await userDb.insert(newUser)
+    try {
+        const added = await userDb.insert(newUser);
 
-// })
+        if (added) {
+            res.status(201).json('New User added.')
+        }
+        else {
+            res.status(404).json('Post id is unavailable.')
+        }
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+
+})
 
 router.get('/:userId', async (req, res) => {
-
-
 
     try {
         // const user = await userDb.getById(req.params.userId);
