83b28056fc9e1235b531487628a891ab169da026
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-25 21:34:58 -0400

    fix storag specs

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
         cb(null, './uploads/')
     },
     filename: function(req, file, cb) {
-        cb(null, file.originalname + '-' + new Date().toISOString());
+        cb(null, new Date().toISOString() + '+' + file.originalname );
         console.log(file)
     }
 
