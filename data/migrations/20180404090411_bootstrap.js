26f05e146d0cb3941ff59a30064a06ed0381d5ee
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-08 19:10:08 -0400

    add roles seeding &  migrations, fix users name in migrx

--- data/migrations/20180404090411_bootstrap.js
+++ data/migrations/20180404090411_bootstrap.js
@@ -1,12 +1,24 @@
 exports.up = function(knex) {
   return knex.schema
+    .createTable('roles', function(roles) {
+      roles.increments();
+      roles.text('name', 25).notNullable().unique()
+    })
     .createTable('users', function(users) {
       users.increments();
       users
-        .text('name', 255).notNullable().unique()
+        .text('username', 255).notNullable().unique()
       users
         .text('password', 25).notNullable();
+      users
+        .integer('role_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('roles')
+
     })
+
     .createTable('categories', function(categories) {
       categories.increments();
       categories
@@ -14,6 +26,7 @@ exports.up = function(knex) {
         .notNullable()
         .unique();  
     })
+
     .createTable('posts', function(posts) {
       posts.increments();
       posts.text('title', 5000).notNullable();
--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -1,13 +1,13 @@
 exports.seed = function(knex, Promise) {
   return knex('users').insert([
-    { name: 'Frodo Baggings', password: 'frodopass' }, // 1
-    { name: 'Samwise Gamgee', password: 'sampass' }, // 2
-    { name: 'Meriadoc Brandybuck', password: 'meriadocpass' }, // 3
-    { name: 'Peregrin Took', password: 'peregrinpass' }, // 4
-    { name: 'Mithrandir', password: 'mithrandirpass' }, // 5
-    { name: 'Boromir', password: 'boromirpass' }, // 6
-    { name: 'Legolas', password: 'legolaspass' }, // 7
-    { name: 'Gimly', password: 'gimlypass' }, // 8
-    { name: 'Aragorn', password: 'aragornpass' }, // 9
+    { username: 'Frodo', password: 'frodopass', role_id: '1' }, // 1
+    { username: 'Samwise', password: 'samwisepass', role_id: '3' }, // 2
+    { username: 'Meriadoc', password: 'meriadocpass', role_id: '4' }, // 3
+    { username: 'Peregrin', password: 'peregrinpass', role_id: '1' }, // 4
+    { username: 'Mithrandir', password: 'mithrandirpass', role_id: '4' }, // 5
+    { username: 'Boromir', password: 'boromirpass', role_id: '2' }, // 6
+    { username: 'Legolas', password: 'legolaspass', role_id: '3' }, // 7
+    { username: 'Gimly', password: 'gimlypass', role_id: '3' }, // 8
+    { username: 'Aragorn', password: 'aragornpass', role_id: '4' }, // 9
   ]);
 };
