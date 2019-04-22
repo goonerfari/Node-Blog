7bc849ab694852bd29a1da89fa1ff2fe5e6feabf
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-22 19:51:37 -0400

    fix another error

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -22,7 +22,7 @@ router.post('/register', (req, res) => {
     console.log(username);
     console.log(password);
     console.log(bcrypt.compareSync(password, user.password))
-    console.log(users).$log([keyPath-optional])
+    console.log(users)
     users.findBy({ username })
       .first()
       .then(user => {
