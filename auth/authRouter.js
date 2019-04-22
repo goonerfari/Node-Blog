091790924a77c2c4baed99eec037b0df7aa04aad
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-22 17:01:08 -0400

    logs in post login

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -19,13 +19,14 @@ router.post('/register', (req, res) => {
   
   router.post('/login', (req, res) => {
     let { username, password } = req.body;
+    console.log(username);
+    console.log(password);
+    console.log(bcrypt.compareSync(password, user.password))
     users.findBy({ username })
       .first()
       .then(user => {
-        console.log(user);
         if (user && bcrypt.compareSync(password, user.password)) {
           const token = generateToken(user);
-          const user = user;
           res.status(200).json({
             token, message: `Welcome ${user.username}!`, user
           });
