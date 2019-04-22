e70d9da7908b1b5a50d417756f1a54a58babdb3c
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-22 14:50:03 -0400

    refactor user object in login response

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -19,15 +19,14 @@ router.post('/register', (req, res) => {
   
   router.post('/login', (req, res) => {
     let { username, password } = req.body;
-    let user = {}
     users.findBy({ username })
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
           const token = generateToken(user);
-          user = user;
+          const user = user;
           res.status(200).json({
-            token, message: `Welcome ${user.username}!`, user,
+            token, message: `Welcome ${user.username}!`, user
           });
         } else {
           res.status(401).json({ message: 'Invalid Credentials' });
